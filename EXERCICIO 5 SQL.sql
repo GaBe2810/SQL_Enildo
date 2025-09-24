81115d14ea09e24b3365d399bcbdbe9e2cda54ff
Gabriel Barbosa Soares <162936568+GaBe2810@users.noreply.github.com>
2025-09-24 11:01:43 -0300

    Add files via upload

---
+++ EXERCICIO 5 SQL.sql
@@ -0,0 +1,24 @@
+create database NomeMae_db;
+show databases;
+show tables;
+
+use NomeMae_db;
+create table tbvenda(
+NF int auto_increment primary key,
+DataValidade datetime not null
+);
+
+describe tbvenda;
+describe produto;
+
+alter table tbvenda add preco decimal(6,2) not null;
+alter table tbvenda add qtd smallint, add check(qtd > 0);
+alter table tbvenda drop DataValidade;
+alter table tbvenda add DataVenda datetime default current_timestamp();
+
+create table produto(
+CodigoB decimal(13,0) primary key,
+NomeProd varchar(50) not null
+);
+
+alter table tbvenda add CodigoB decimal(13,0), add foreign key(CodigoB) references produto (CodigoB);
